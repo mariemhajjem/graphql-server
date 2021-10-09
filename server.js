ea7680807997af27405929374887d3296f0c350b
mariem <mariemhajjem10@gmail.com>
2021-10-09 08:46:03 +0100

    add a graphql endpoint

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express()
+const cors = require('cors')
+const graphqlHTTP = require('express-graphql')
+const {schema} = require('./graphql/graphql-schema')
+
+
+const port = 5000;
+
+router.use(express.json());
+router.use(cors());
+router.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql : true
+}))
+
+router.listen(port, () => console.log(`server is running on port ${port}`))
